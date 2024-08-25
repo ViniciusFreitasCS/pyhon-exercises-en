3083d0a93d06a6a4e2a9f06c8e88fc2cbde41dbc
Vinícius Freitas <102237421+ViniciusFreitasCS@users.noreply.github.com>
2024-08-25 16:44:56 -0400

    Update 008_ConversãoMétrica.py

--- 008_ConversãoMétrica.py
+++ 008_ConversãoMétrica.py
@@ -1,9 +1,9 @@
 # Converter metros em centímetros e milímetros
 
-metros = int(input('Valor em metros: '))
+metros = float(input('Valor em metros: '))
 
 cent = metros * 100
 
 mili = metros * 1000
 
-print('Metros (m): {}\nCentímetros (cm): {}\nMilímetros (mm): {}'.format(metros, cent, mili))
+print(f'Metros (m): {metros}\nCentímetros (cm): {cent}\nMilímetros (mm): {mili}')
